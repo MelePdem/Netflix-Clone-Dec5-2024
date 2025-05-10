904d73e84dce25552e180b466c42f44e23d0bed0
MelePdem <melesedemessie88@gmail.com>
2025-05-10 04:00:38 -0400

    My First Commit is just Started

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='app'>
+        <h1> Hello!Now you can start building netflix </h1>
+        
+     </div>
+     
+    </>
+  )
+}
+
+export default App
