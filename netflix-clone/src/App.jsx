034aa56f7fbc02a3842979620595e04287cf091a
MelePdem <melesedemessie88@gmail.com>
2025-05-12 08:17:46 -0400

    Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div className='app'>
-        <h1> Hello!Now you can start building netflix </h1>
-        
-     </div>
-     
-    </>
+         <>
+      <Header />
+      <Footer/>
+         </>
   )
 }
 
---
+++ netflix-clone/src/Components/Footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import FacebookSharpIcon from '@mui/icons-material/FacebookSharp';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className="outer">
+      <div className="inner">
+        <div className="footer_icon">
+          <FacebookSharpIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="footer_data">
+          <div>
+            <ul>
+              <li>AudioDescription</li>
+              <li>Investor Relatioons</li>
+              <li>LegalNotice</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>HelpCentre</li>
+              <li>Jobs</li>
+              <li>Cookie Performance</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>GiftCards</li>
+              <li>Terms of use</li>
+              <li>CorporateInformation</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>MediaCentre</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer
